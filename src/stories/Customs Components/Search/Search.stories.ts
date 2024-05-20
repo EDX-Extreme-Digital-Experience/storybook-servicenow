1c0da12418f17ef29eab6074c2f6055e11089222
Gustavo Tardin de Paula <gustavofilipetardin@outlook.com>
2024-05-20 18:26:01 -0300

    feat: Sucessfully config all initial files, now it´s time to code!

---
+++ src/stories/Customs Components/Search/Search.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Search from './Search';
+
+const meta = {
+    title: 'Components/Search Bar',
+    component: Search,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof Search>;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultSearch: Story = {
+    args: {
+        Placeholder: 'PlaceHolder',
+        Scale: 'Default',
+        State: 'Default',
+        CloseIcon: false,
+        SearchIcon: true,
+        SearchPlaceHolder: '',
+        Shape: 'Default',
+    }
+}
+
+export default meta
