ede5ac92cefcdb0ec1ffc2c6f1c075253f9f8d6a
marcelachristine <marcelachristine.melo@gmail.com>
2024-06-19 14:57:35 -0300

    Criação do componente Attachments

--- src/stories/Customs Components/Attachments/Attachments.stories.ts
+++ src/stories/Customs Components/Attachments/Attachments.stories.ts
@@ -1,35 +1,24 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { Meta, Story } from '@storybook/react';
 import { Attachments } from './Attachments';
 
-
 const meta = {
-  title: 'Components/Attachments',
-  component: Attachments,
-  parameters: {
-    
-    layout: 'centered',
-  },
-  
-  tags: ['autodocs'],
+    title: 'Components/Attachments',
+    component: Attachments,
+    parameters: {
 
- 
+    layout: 'centered',
+},
+
+    tags: ['autodocs'],
+
+
 } satisfies Meta<typeof Attachments>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type StoryType = Story<typeof meta>;
 
-export const Attachment: Story = {
-  args: {
-    state: 'Success',
-    outlined: false,
-    dismissible: false,
-    title: 'Esta é uma mensagem de informação',
-    description: true,
-    descriptionText: 'Esta é a descrição da informação.',
-    secondAction: true,
-    actions: true,
-  },
+export const AttachmentsProps: StoryType = {
+args: {
+    hover: false,
+},
 };
-
-  
-  
--- src/stories/Customs Components/Attachments/Attachments.tsx
+++ src/stories/Customs Components/Attachments/Attachments.tsx
@@ -2,31 +2,33 @@ import './attachments.css';
 import iconDonwload from '../../assets/download.png';
 import iconDismiss from '../../assets/x.png';
 
-export interface Attachment {
-    state?: 'Success' | 'Error' | 'Warning' | 'Info',
-    outlined: boolean,
-    dismissible: boolean,
-    title: string,
-    description: boolean,
-    descriptionText: string,
-    secondAction: boolean,
-    actions: boolean,           
+export interface Attachments {
+hover: boolean;
 }
 
-export const Attachments = () => {
-    return(
-    <div className="attachment">
-        <div className='attachment-header'>
-            <h3>IMG_0378.jpg  (1.3 MB)</h3>
+export const Attachments: React.FC<Attachments> = ({ hover }) => {
+return (
+<div className="attachment">
+    <div className="attachment-header">
+        <p>IMG_0378.jpg  (1.3 MB)</p>
+        <div className="descricao">
+            <p>25d atrás</p>
         </div>
-        <div className='teste'>
-            <button className='button1'>
-            <img src={iconDonwload} alt="Download"/>
-            </button>
-            <button className='button2'>
-            <img src={iconDismiss} alt="Dismiss"/>
+    </div>
+    <div className="content-tooltips">
+        <div>
+        <button className={`button1 ${hover ? 'hover' : ''}`} data-tooltip="Baixar">
+                <img src={iconDonwload} alt="Download"/>
+        </button>
+        </div>
+        <div>
+            <button className="button2" data-tooltip="Deletar">
+                <img src={iconDismiss} alt="Dismiss"/>
             </button>
         </div>
     </div>
-    )
-}
+</div>
+);
+};
+
+
