2fd22b48b1241cc6ebf53637ca454793d2cc30df
marcelachristine <marcelachristine.melo@gmail.com>
2024-07-30 15:56:17 -0300

    Desenvolvimento do Componente Empty State

--- src/stories/Customs Components/EmptyState/EmptyState.stories.ts
+++ src/stories/Customs Components/EmptyState/EmptyState.stories.ts
@@ -5,12 +5,15 @@ const meta = {
   title: 'Components/Empty State',
   component: EmptyState,
   parameters: {
-    
     layout: 'centered',
   },
-  
   tags: ['autodocs'],
-
+  argTypes: {
+    type: {
+      control: 'radio',
+      options: ['Ilustration1', 'Ilustration2', 'Ilustration3', 'Ilustration4', 'Ilustration5'],
+    },
+  },
 } satisfies Meta<typeof EmptyState>;
 
 export default meta;
@@ -18,6 +21,9 @@ type Story = StoryObj<typeof meta>;
 
 export const EmptyStates: Story = {
   args: {
+    text: 'Desculpe, não foram encontrados resultados para essa busca',
+    description: 'Verifique os termos digitados e tente novamente',
+    type: 'Ilustration1',
     buttonLabel: 'Voltar para a página anterior',
     buttonHover: false,
   },
--- src/stories/Customs Components/EmptyState/EmptyState.tsx
+++ src/stories/Customs Components/EmptyState/EmptyState.tsx
@@ -1,27 +1,54 @@
 import './emptyState.css';
 import { Button } from '../Button/Button';
-import Ilustration from '../../assets/Ilustration.png';
+import Ilustration1 from '../../assets/Ilustration1.png';
+import Ilustration2 from '../../assets/Ilustration2.png';
+import Ilustration3 from '../../assets/Ilustration3.png';
+import Ilustration4 from '../../assets/Ilustration4.png';
+import Ilustration5 from '../../assets/Ilustration5.png';
 
-export interface EmptyStates {
-  buttonLabel: string;
-  buttonHover: boolean,
-}
 
-export const EmptyState: React.FC<EmptyStates> = ({ buttonLabel, buttonHover }) => {
-return (
-  <div className='contents'>
-      <div className="content-img">
-        <img src={Ilustration} alt="Ilustração" />
-      </div>
-      <div className='content-text'>
-        <h2>Desculpe, não foram encontrados resultados para essa busca</h2>
-        <p>Verifique os termos digitados e tente novamente</p>
-      </div>
-      <div className='content-button'>
-        <Button label={buttonHover ? buttonLabel : buttonLabel} typeButton="Primary" leftIcon={false} rightIcon={false} style={{ backgroundColor: buttonHover ? 'var(--button-primary-hover, #182CB1)' : ''}}/>    
-      </div>  
-  </div>
-);
+const illustrations = {
+  Ilustration1,
+  Ilustration2,
+  Ilustration3,
+  Ilustration4,
+  Ilustration5
 };
 
+export interface EmptyStatesProps {
+  text: string;
+  description: string;
+  type: 'Ilustration1' | 'Ilustration2' | 'Ilustration3' | 'Ilustration4' | 'Ilustration5';
+  buttonLabel: string;
+  buttonHover: boolean;
+}
+
+export const EmptyState: React.FC<EmptyStatesProps> = ({
+  text,
+  description,
+  type,
+  buttonLabel,
+  buttonHover,
+}) => {
+  return (
+    <div className='contents'>
+      <div className="content-img">
+        <img src={illustrations[type]} alt="Ilustração" />
+      </div>
+      <div className='content-text'>
+        <h2>{text}</h2>
+        <p>{description}</p>
+      </div>
+      <div className='content-button'>
+        <Button
+          label={buttonLabel}
+          typeButton="Primary"
+          leftIcon={false}
+          rightIcon={false}
+          style={{ backgroundColor: buttonHover ? 'var(--button-primary-hover, #182CB1)' : ''}}
+        />
+      </div>
+    </div>
+  );
+};
 
